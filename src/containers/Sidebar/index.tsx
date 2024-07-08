2f5e0a3311ededea81ac4746f9fa08e67bd45af2
Alex <alexfaustinosouza@gmail.com>
2024-07-07 23:32:26 -0400

    estilazando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https:/avatars.githubusercontent.com/u/134724259?v=4" />
-    <Titulo fontSize={20}>Alex Faustino</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Alex Faustino</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Alexfs-dev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
